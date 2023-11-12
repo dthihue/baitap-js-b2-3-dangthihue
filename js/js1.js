bb010ca6bed66684e6f5a35b6ca3156c0635b36f
Đặng Thị Huế <148392779+dthihue@users.noreply.github.com>
2023-11-13 02:06:56 +0700

    first commit

---
+++ js/js1.js
@@ -0,0 +1,86 @@
+/* bài1:
+mô hình 3 khối:
+Khối1:
+input: tiền lương 1 ngày 100.000 : number
+      - số ngày làm: number
+
+Khối 2:
+Khối 3:
+
+*/
+
+function Tinhtongluong() {
+  var tienluong1ngay = document.getElementById("luong1ngay").value;
+  var songaylam = document.getElementById("songaylam").value;
+  //kiểm tra
+  console.log("tiền lương 1 ngày", tienluong1ngay);
+  console.log("số ngày làm", songaylam);
+  var tongluong = 0;
+  tongluong = tienluong1ngay * songaylam;
+  document.getElementById("tongluong").innerHTML = tongluong;
+}
+
+// *bài 2
+/*
+mô hình 3 khối:
+input:
+-nhập 5 giá trị
+-tính toán
+output: 
+tính giá trị trung bình và xuất ra màn hình  
+*/
+function Nhaplieu() {
+  var so1 = document.getElementById("number1").value;
+  var so2 = document.getElementById("number2").value;
+  var so3 = document.getElementById("number3").value;
+  var so4 = document.getElementById("number4").value;
+  var so5 = document.getElementById("number5").value;
+  var Giatri = 5;
+
+  console.log("so1", so1);
+  console.log("so2", so2);
+  console.log("so3", so3);
+  console.log("so4", so4);
+  console.log("so5", so5);
+  console.log("giá trị", Giatri);
+
+  var Tong = so1 + so2 + so3 + so4 + so5 / Giatri;
+  console.log("tong", Tong);
+  document.getElementById("Trungbinh").innerHTML = Tong;
+}
+
+// --bài 3--//
+function trans() {
+  var usd = document.getElementById("usd").value;
+  var vnd = document.getElementById("vnd").value;
+  console.log("usd", usd);
+  console.log("vnd", vnd);
+  var tongchuyendoi = usd * vnd;
+  console.log("tongchuyendoi", tongchuyendoi);
+  document.getElementById("Xuat").innerHTML = tongchuyendoi;
+}
+
+// bài 4 //
+
+function HCN() {
+  var dai = document.getElementById("dai").value;
+  var rong = document.getElementById("rong").value;
+  console.log("dai", dai);
+  console.log("rong", rong);
+  var dientich = dai * rong;
+  var chuvi = (dai + rong) * 2;
+  console.log("dientich", dientich);
+  console.log("chuvi", chuvi);
+  document.getElementById("tongDT").innerHTML = dientich;
+  document.getElementById("tongCV").innerHTML = chuvi;
+}
+
+
+// ----bài 5----
+var so = 86;
+var tongkyso = 0;
+var hangchuc = math.floor((so)/10);
+var hangdonvi = math.floor((so)%10);
+tongkyso = hangchuc + donvi;
+console.log('tongkyso',tongkyso);
+
